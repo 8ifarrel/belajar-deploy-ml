16e5481d73c5235aeec61d54981f5804f3b982df
8ifarrel <farrelsirah@gmail.com>
2024-12-27 21:55:34 +0800

    baikin url

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -1,5 +1,5 @@
 const tf = require('@tensorflow/tfjs-node');
 async function loadModel() {
-    return tf.loadGraphModel('https://storage.cloud.google.com/submissionmlgc-8ifarrel-bucket/model.json');
+    return tf.loadGraphModel('https://storage.googleapis.com/submissionmlgc-8ifarrel-bucket/model.json');
 }
 module.exports = loadModel;
--- src/services/storeData.js
+++ src/services/storeData.js
@@ -1,13 +1,13 @@
 const { Firestore } = require('@google-cloud/firestore');
 const path = require('path');
 
-const pathKey = path.resolve('./open-music-api-fcacdf2ceca6.json')
+const pathKey = path.resolve('./credential.json')
      
 async function storeData(id, data) {
 
   try {
     const db = new Firestore({
-      projectId: 'open-music-api',
+      projectId: 'submissionmlgc-8ifarrel',
       keyFilename: pathKey,
     });
  
